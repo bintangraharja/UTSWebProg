6fab929da683d7499bbb71df4cc0cf55f8cbf1ab
ferrylay <ferry.lay@student.umn.ac.id>
2021-03-13 15:34:59 +0800

    revisi dikit

--- Controller/OrderController.php
+++ Controller/OrderController.php
@@ -1,21 +1,24 @@
 <?php
     session_start();
-    if(!isset($_SESSION['user-login'])){
+    if($_SESSION['user-login'] == 'user'){
+        if(isset($_POST['submit'])){
+            include("../Include/db_config.php");
+            $IDUser = $_SESSION['iduser'];
+            $IDMenu = $_POST['menuID'];
+            $NamaMenu = $_POST['namaMenu'];
+            $Qty = $_POST['qty'];
+            $harga = $_POST['total'];
+            $url = $_GET['url'];
+    
+            $query = "INSERT INTO pesanan(IDUser,IDMenu,NamaMenu,Qty,Harga) 
+            VALUES ('$IDUser', '$IDMenu','$NamaMenu','$Qty','$harga')";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            header("Location:../View/$url.php");
+        }
+    }
+    else{
         header("Location: ../View/LogIn.php");
     }
-    if(isset($_POST['submit'])){
-        include("../Include/db_config.php");
-        $IDUser = $_SESSION['iduser'];
-        $IDMenu = $_POST['menuID'];
-        $NamaMenu = $_POST['namaMenu'];
-        $Qty = $_POST['qty'];
-        $harga = $_POST['total'];
-        $url = $_GET['url'];
-
-        $query = "INSERT INTO pesanan(IDUser,IDMenu,NamaMenu,Qty,Harga) 
-        VALUES ('$IDUser', '$IDMenu','$NamaMenu','$Qty','$harga')";
-        $stmt = $db->prepare($query);
-        $stmt->execute();
-        header("Location:../View/$url.php");
-    }
+    
 ?>
